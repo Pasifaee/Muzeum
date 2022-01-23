24a1a31404f76bb2cabaff46df51bb660da3221f
Pasifaee <izabelaozdz@gmail.com>
2022-01-23 18:31:18 +0100

    Template bazowy z działającym linkiem do strony admina + Strona główna

--- muzeum/views.py
+++ muzeum/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Artysta, Eksponat, Ekspozycja, Wypozyczenie
 
-# Create your views here.
+
+def index(request):
+    """Home page"""
+    return render(request, 'index.html')
