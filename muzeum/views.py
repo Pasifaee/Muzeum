ed4b39dfe8d4985a2c1354ad3f5e06ee5552f6ec
Pasifaee <izabelaozdz@gmail.com>
2022-01-23 19:17:10 +0100

    Strona "Artyści" wypisuje wszystkich artystów w bazie.

--- muzeum/views.py
+++ muzeum/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Artysta, Eksponat, Ekspozycja, Wypozyczenie
 
 
 def index(request):
-    """Home page"""
+    """Strona główna"""
     return render(request, 'index.html')
+
+class ArtistListView(generic.ListView):
+    model = Artysta
+    context_object_name = 'artist_list'
+    template_name = 'muzeum/artysci.html'
