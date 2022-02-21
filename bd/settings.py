5ab710ec8d0fca17714ef0fb589c5609f816bb37
Agata <agata.bielenica@gmail.com>
2022-02-21 23:24:24 +0100

    Zmiany w bazie danych

--- bd/settings.py
+++ bd/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6amp(=hz)2alk8g#3q=19gg0ivkrsf8hraiozyw_*2b61y4ha#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
